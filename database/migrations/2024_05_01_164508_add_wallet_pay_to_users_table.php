05320da44cbd1c1adfd3aa719a3d5df3ce3b737a
ViraCodingPlus <viracodingplus@gmail.com>
2024-05-02 00:09:54 +0330

    add table migration

---
+++ database/migrations/2024_05_01_164508_add_wallet_pay_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('wallet_pay')->nullable()->after('wallet_gift');
+            $table->timestamp('wallet_pay_expire')->nullable()->after('wallet_pay');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('wallet_pay');
+            $table->dropColumn('wallet_pay_expire');
+        });
+    }
+};
